6d252a6cf0dc429c8b5c9370b0b5db0d24b98adc
Yuqi Jin <jinyq1997@gmail.com>
2021-01-13 22:11:43 +0000

    read yaml inside js

--- functions/flare-download-noaa/function.js
+++ functions/flare-download-noaa/function.js
@@ -5,6 +5,8 @@ const fs = require('fs');
 const path = require('path');
 const shell = require('shelljs');
 const cp = require('child_process');
+const yaml = require('js-yaml');
+const { config } = require('process');
 
 app.use(bodyParser.json());
 
@@ -19,29 +21,55 @@ app.post('/init', function (req, res) {
 
 app.post('/run', function (req, res) {
     var payload = (req.body || {}).value;
-    let ret="";
+    let ret = "";
+    let prerunpull = "FALSE";
+    let postrunpush = "FALSE";
+
 
     shell.echo(payload.ssh_key.join('\n')).to('/code/id_rsa');   
 
     shell.exec(`wget -O - https://raw.githubusercontent.com/FLARE-forecast/FLARE-containers/commons/flare-install.sh | /usr/bin/env bash -s ${payload.container_name}`);
 
+    fs.readFile(`/opt/flare/shared/${payload.container_name}/flare-config.yml`, 'utf8', function (e, data) {
+        var file;
+        if (e) {
+            console.log('flare-config.yml not found.');
+        } else {
+            file = yaml.safeLoad(data, 'utf8');
+            prerunpull = file.container.working-directory.pre-run-pull;
+            postrunpush = file.container.working-directory.post-run-push;
+        }
+    });
+
     const process1 = cp.spawnSync('/bin/bash', ['/code/flare_pullworkdir.sh', `${payload.gitlab_server}`, `${payload.gitlab_port}`, `${payload.lake}`, `${payload.container_name}`, `${payload.username}`], { stdio: 'inherit' });
-    if(!process1.status){
+    if(!process1.status){ 
         const process2 = cp.spawnSync('/bin/bash', [`/opt/flare/${payload.container_name}/flare-host.sh`, '-d', '--openwhisk'], { stdio: 'inherit' });
         if(!process2.status){
-            const process3 = cp.spawnSync('/bin/bash', ['/code/flare_pushworkdir.sh', `${payload.gitlab_server}`, `${payload.gitlab_port}`, `${payload.lake}`, `${payload.container_name}`, `${payload.username}`], { stdio: 'inherit' });
-            if(!process3.status){
+            if (postrunpush != "FALSE") {
+                const process3 = cp.spawnSync('/bin/bash', ['/code/flare_pushworkdir.sh', `${payload.gitlab_server}`, `${payload.gitlab_port}`, `${payload.lake}`, `${payload.container_name}`, `${payload.username}`], { stdio: 'inherit' });
+                if(!process3.status){
+                    const process4 = cp.spawnSync('/bin/bash', ['/code/flare_triggernext.sh', `${payload.container_name}`], { stdio: 'inherit' });
+                    if(!process4.status){
+                        ret += "success";
+                    }
+                    else{
+                        ret += "error in running flare_triggernext.sh; ";
+                    }    
+                }
+                else{
+                    ret += "error in running flare_pushworkdir.sh; ";
+                }
+            }
+            else{
                 const process4 = cp.spawnSync('/bin/bash', ['/code/flare_triggernext.sh', `${payload.container_name}`], { stdio: 'inherit' });
                 if(!process4.status){
                     ret += "success";
                 }
                 else{
                     ret += "error in running flare_triggernext.sh; ";
-                }    
+                }  
             }
-            else{
-                ret += "error in running flare_pushworkdir.sh; ";
-            }
+
         }
         else{
             ret += "error in running flare-host.sh; ";
