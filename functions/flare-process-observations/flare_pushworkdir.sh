2c0a5e59738a6e513e8aef3f4a4b053c89101b1b
Yuqi Jin <jinyq1997@gmail.com>
2021-02-18 00:16:42 +0000

    update new naming scheme

--- functions/flare-process-observations/flare_pushworkdir.sh
+++ functions/flare-process-observations/flare_pushworkdir.sh
@@ -19,8 +19,8 @@ TIMESTAMP=$(date +"%Y%m%d")
 mkdir ~/.ssh/
 cp /code/id_rsa ~/.ssh/id_rsa
 chmod 400 ~/.ssh/id_rsa
-ssh-keyscan -t rsa ${GITLAB_SERVER} >> ~/.ssh/known_hosts
 
+ssh-keyscan -t rsa ${GITLAB_SERVER} >> ~/.ssh/known_hosts
 tar -czvf ${LAKE}_${TIMESTAMP}_${CONTAINER}_workingdirectory.tar.gz -C ${DIRECTORY_CONTAINER_SHARED} ${CONTAINER}
 scp ${LAKE}_${TIMESTAMP}_${CONTAINER}_workingdirectory.tar.gz ubuntu@${GITLAB_SERVER}:/home/ubuntu/fcre/${CONTAINER}/
 rm ${LAKE}_${TIMESTAMP}_${CONTAINER}_workingdirectory.tar.gz
--- functions/flare-visualize/flare_pushworkdir.sh
+++ functions/flare-visualize/flare_pushworkdir.sh
@@ -15,12 +15,12 @@ LAKE=$3             #third argument
 CONTAINER=$4        #fourth argument
 USERNAME=$5         #fifth argument
 
-TIMESTAMP=$(date +"%d_%m_%y")
+TIMESTAMP=$(date +"%Y%m%d")
 mkdir ~/.ssh/
 cp /code/id_rsa ~/.ssh/id_rsa
 chmod 400 ~/.ssh/id_rsa
-ssh-keyscan -t rsa ${GITLAB_SERVER} >> ~/.ssh/known_hosts
 
-tar -czvf ${CONTAINER}-output.tar.gz -C ${DIRECTORY_CONTAINER_SHARED} ${CONTAINER}
-scp ${CONTAINER}-output.tar.gz ubuntu@${GITLAB_SERVER}:/home/ubuntu/fcre/${CONTAINER}/
-rm ${CONTAINER}-output.tar.gz
+ssh-keyscan -t rsa ${GITLAB_SERVER} >> ~/.ssh/known_hosts
+tar -czvf ${LAKE}_${TIMESTAMP}_${CONTAINER}_workingdirectory.tar.gz -C ${DIRECTORY_CONTAINER_SHARED} ${CONTAINER}
+scp ${LAKE}_${TIMESTAMP}_${CONTAINER}_workingdirectory.tar.gz ubuntu@${GITLAB_SERVER}:/home/ubuntu/fcre/${CONTAINER}/
+rm ${LAKE}_${TIMESTAMP}_${CONTAINER}_workingdirectory.tar.gz
