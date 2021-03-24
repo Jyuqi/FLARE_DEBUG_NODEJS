3aeac26bec8559a574bd2afe172aae3f5325d849
Jyuqi <jinyuqi@ufl.edu>
2021-03-24 16:23:16 +0000

    update workdir script

--- functions/commons/flare_pullworkdir.sh
+++ functions/commons/flare_pullworkdir.sh
@@ -11,22 +11,34 @@ DIRECTORY_HOST="/opt/flare"
 DIRECTORY_HOST_SHARED="/opt/flare/shared"
 DIRECTORY_CONTAINER_SHARED="/root/flare/shared"
 CONFIG_FILE="flare-config.yml"
-STORAGE_SERVER=$1
-LAKE=$2 
-CONTAINER=$3  
+# STORAGE_SERVER=$1
+# LAKE=$2 
+# CONTAINER=$3  
+s3_endpoint=$(yq r ${CONFIG_FILE} openwhisk.s3_storage.s3_endpoint)
+s3_access_key=$(yq r ${CONFIG_FILE} openwhisk.s3_storage.s3_access_key)
+s3_secret_key=$(yq r ${CONFIG_FILE} openwhisk.s3_storage.s3_secret_key)
+LAKE=$(yq r ${CONFIG_FILE} lake_name_code)
+CONTAINER=$(yq r ${CONFIG_FILE} container.name)
+Ndays_steps=$(yq r ${CONFIG_FILE} openwhisk.days-look-back)
+set_of_dependencies=$(yq r ${CONFIG_FILE} openwhisk.container-dependencies)
+current_date=$(date +%Y%m%d)
 
 mkdir -p ~/.ssh/
 cp /code/id_rsa ~/.ssh/id_rsa
 chmod 400 ~/.ssh/id_rsa
 
-ssh-keyscan -t rsa ${STORAGE_SERVER} >> ~/.ssh/known_hosts
+# ssh-keyscan -t rsa ${STORAGE_SERVER} >> ~/.ssh/known_hosts
+
 # copy config file
-scp ubuntu@${STORAGE_SERVER}:/home/ubuntu/fcre/${CONTAINER}/${CONFIG_FILE} ${DIRECTORY_HOST_SHARED}/${CONTAINER}/ || error_exit "$LINENO: An error has occurred in copy config file."
+# scp ubuntu@${STORAGE_SERVER}:/home/ubuntu/fcre/${CONTAINER}/${CONFIG_FILE} ${DIRECTORY_HOST_SHARED}/${CONTAINER}/ || error_exit "$LINENO: An error has occurred in copy config file."
+cp /code/${CONFIG_FILE} ${DIRECTORY_HOST_SHARED}/${CONTAINER}/
+
+
+mc alias set s3_flare $s3_endpoint $s3_access_key $s3_secret_key
+
 
 # copy work dir
-Ndays_steps=$(yq r ${DIRECTORY_HOST_SHARED}/${CONTAINER}/${CONFIG_FILE} openwhisk.days-look-back)
-set_of_dependencies=$(yq r ${DIRECTORY_HOST_SHARED}/${CONTAINER}/${CONFIG_FILE} openwhisk.container-dependencies)
-current_date=$(date +%Y%m%d)
+
 
 for FLARE_CONTAINER_NAME in ${set_of_dependencies};
 do
@@ -36,7 +48,7 @@ do
     	scandate=$(date -d "$current_date - $daysback days" +%Y%m%d)
 		if (downloaded==false) 
 		then
-			scp ubuntu@${STORAGE_SERVER}:/home/ubuntu/fcre/$FLARE_CONTAINER_NAME/${LAKE}_${scandate}_${FLARE_CONTAINER_NAME}_workingdirectory.tar.gz ${DIRECTORY_HOST_SHARED}/
+			mc cp flare/${LAKE}/$FLARE_CONTAINER_NAME/${LAKE}_${scandate}_${FLARE_CONTAINER_NAME}_workingdirectory.tar.gz ${DIRECTORY_HOST_SHARED}/
 			if [ "$?" -eq "0" ]; # copy work dir success
 			then
 				echo "OK"
--- functions/commons/flare_pushworkdir.sh
+++ functions/commons/flare_pushworkdir.sh
@@ -9,9 +9,15 @@ error_exit()
 
 DIRECTORY_CONTAINER_SHARED="/root/flare/shared"
 DIRECTORY_HOST="/opt/flare"
-STORAGE_SERVER=$1    #first argument
-LAKE=$2   
-CONTAINER=$3 
+CONFIG_FILE="flare-config.yml"
+# STORAGE_SERVER=$1    #first argument
+# LAKE=$2   
+# CONTAINER=$3 
+s3_endpoint=$(yq r ${CONFIG_FILE} openwhisk.s3_storage.s3_endpoint)
+s3_access_key=$(yq r ${CONFIG_FILE} openwhisk.s3_storage.s3_access_key)
+s3_secret_key=$(yq r ${CONFIG_FILE} openwhisk.s3_storage.s3_secret_key)
+LAKE=$(yq r ${CONFIG_FILE} lake_name_code)
+CONTAINER=$(yq r ${CONFIG_FILE} container.name)
 
 
 TIMESTAMP=$(date +"%Y%m%d")
@@ -21,5 +27,6 @@ chmod 400 ~/.ssh/id_rsa
 
 ssh-keyscan -t rsa ${STORAGE_SERVER} >> ~/.ssh/known_hosts
 tar -czvf ${LAKE}_${TIMESTAMP}_${CONTAINER}_workingdirectory.tar.gz -C ${DIRECTORY_CONTAINER_SHARED} ${CONTAINER}
-scp ${LAKE}_${TIMESTAMP}_${CONTAINER}_workingdirectory.tar.gz ubuntu@${STORAGE_SERVER}:/home/ubuntu/fcre/${CONTAINER}/
+# scp ${LAKE}_${TIMESTAMP}_${CONTAINER}_workingdirectory.tar.gz ubuntu@${STORAGE_SERVER}:/home/ubuntu/fcre/${CONTAINER}/
+mc cp ${LAKE}_${TIMESTAMP}_${CONTAINER}_workingdirectory.tar.gz flare/${LAKE}/$FLARE_CONTAINER_NAME/
 rm ${LAKE}_${TIMESTAMP}_${CONTAINER}_workingdirectory.tar.gz
