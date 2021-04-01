7dd5dd53d4ed4b57ad8bbc1c265d2322388f79f6
Jyuqi <jinyuqi@ufl.edu>
2021-04-01 04:21:14 +0000

    remove blank line

--- functions/commons/flare_pushworkdir.sh
+++ functions/commons/flare_pushworkdir.sh
@@ -15,13 +15,10 @@ s3_secret_key=$3
 CONTAINER=$4
 LAKE=$5
 
-
 TIMESTAMP=$(date +"%Y%m%d")
 mkdir -p ~/.ssh/
 cp /code/id_rsa ~/.ssh/id_rsa
 chmod 400 ~/.ssh/id_rsa
-
-
 
 tar -czvf ${LAKE}_${TIMESTAMP}_${CONTAINER}_workingdirectory.tar.gz -C ${DIRECTORY_CONTAINER_SHARED} ${CONTAINER}
 /code/mc cp ${LAKE}_${TIMESTAMP}_${CONTAINER}_workingdirectory.tar.gz flare/${LAKE}/${CONTAINER}/ || error_exit "$LINENO: An error has occurred in push $FLARE_CONTAINER_NAME working directory."
